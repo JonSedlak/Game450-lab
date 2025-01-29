7bf5f27d4c0a062dad469925f719811a01a235c0
JonSedlak <jonathanmsedlak@gmail.com>
2025-01-29 13:46:28 -0500

    GitHub Account got hacked and deleted. Posting this project to my new GitHub.

--- lab03/lab03.md
+++ lab03/lab03.md
@@ -1,2 +1,18 @@
 # Prompt Engineering Process
 
+# Prompt Engineering Process
+
+1. Added user input and have it loop
+2. Loop is broken and cannot enter in user input after 1st try. It keeps using the orginal message over and over unless I force quit.
+3. Fixed the loop isue. Attempts.txt is now generating
+4. Tried adding an intital statement. did not really work
+5. Changed the temperature and system role content to more accurate
+6. Went back to original conditions. Added an if statement to check for original message. Works now
+7. Decided to get rid of If Else statement for initial message. Changed the system content to: You are a DND master
+8. **I now have a better understanding how ollama works and the flow of series of messages**
+
+## DISCLAIMER
+
+### GitHub Account got hacked and deleted
+
+**I had this project locally saved on my machine so I copied the files over and pasted them into a fresh git clone of the project from profremote**
--- lab03/lab03_dnd_agent.py
+++ lab03/lab03_dnd_agent.py
@@ -6,21 +6,30 @@ from ollama import chat
 from util.llm_utils import pretty_stringify_chat, ollama_seed as seed
 
 # Add you code below
-sign_your_name = 'Pulin Agrawal'
-model = ''
-options = {}
-messages = []
+sign_your_name = 'Jonathan Sedlak'
+model = 'llama3.2'
+messages = [{'role': 'system', 'content': 'You are a DND Master'}]
+options = {'temperature': 0.5, 'max_tokens': 100} # Temperature: Low = Accurate, High = Creative
 
 
 # But before here.
 
 options |= {'seed': seed(sign_your_name)}
+
 # Chat loop
 while True:
   response = chat(model=model, messages=messages, stream=False, options=options)
   # Add your code below
+  #-----------------------------------------------------------
 
+  print(f'Agent: {response.message.content}')
+  messages.append({'role': 'assistant', 'content': response.message.content})
 
+  # Prompt for user input
+  message = {'role': 'user', 'content': input('You: ')}
+  messages.append(message)
+
+  #-----------------------------------------------------------
   # But before here.
   if messages[-1]['content'] == '/exit':
     break
@@ -34,4 +43,3 @@ with open(Path('lab03/attempts.txt'), 'a') as f:
   file_string += pretty_stringify_chat(messages)
   file_string += '\n\n\n------------------------END OF ATTEMPT------------------------\n\n\n'
   f.write(file_string)
-
